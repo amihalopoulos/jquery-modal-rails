5eb5b30af584b6ba307e8459626edd03d5023fe7
Alexander Mihalopoulos <amihalop@gmail.com>
2015-08-13 21:23:34 -0400

    remove vertical center

--- vendor/assets/javascripts/jquery.modal.js
+++ vendor/assets/javascripts/jquery.modal.js
@@ -145,7 +145,7 @@
     center: function() {
       this.$elm.css({
         position: 'fixed',
-        top: "20%",
+        top: "0",
         left: "50%",
         // marginTop: - (this.$elm.outerHeight() / 2),
         marginLeft: - (this.$elm.outerWidth() / 2),
